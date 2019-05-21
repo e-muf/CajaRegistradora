d86e87edd7c73d735910151b598139136f8aff4c
Emanuel <mufymes@gmail.com>
2019-05-20 23:49:08 -0500

    Cambiando variables y posicion de memoria

--- CajaRegistradora.asm
+++ CajaRegistradora.asm
@@ -32,83 +32,91 @@ SPCR   EQU   $1028
 CSCTL  EQU   $105D
 OPT2   EQU   $1038
 
+C50    EQU   $0032
+C10    EQU	 $000A
+
 DIR_BASE EQU  $0050
 
 ***********************************
 *     DECLARACION DE VARIABLES    *
 ***********************************
-ORDEN  EQU   $0000	*variable que almacena lo que recibe el puerto serial
-VAR    EQU   $0001
+ORDEN  EQU   $0000	* VARIABLE PARA LEER EL PUERTO SERIAL
+VAR    EQU   $0001  * ESTA NO SE PARA QUE LA VAYAMOS A UTILIZAR
 
-DECA   EQU   $0004	*descuento unitario en A
-A      EQU   $0008	*cantidad de artículos acumulados de A
-AU     EQU   $000C     	*de A1 a A5 son para representar el subtotal en BCD (decenas, centenas, etc)
-AD     EQU   $000D
-AC     EQU   $000E
-AUM    EQU   $000F
-ADM    EQU   $0010
-ACM    EQU   $0011
-AUMI   EQU   $0012
-ADMI   EQU   $001F
+A      EQU   $0202	* CONTADOR DE ARTICULAS 'A'
+AU     EQU	 $0204  * UNIDADES DE 'A'
+AD     EQU   $0205  * DECENAS DE 'A'
+AC     EQU   $0206  * CENTENAS DE 'A'
+TA     EQU   $0207  * TOTAL DE 'A'
+		
+B      EQU   $0212	* CONTADOR DE ARTICULOS 'B'
+BD     EQU	 $0214  * DECENAS DE 'B'
+BC     EQU   $0215  * CENTENAS DE 'B'
+BUM    EQU   $0216  * UNIDADES DE MILLAR DE 'B'
+TB     EQU   $0217  * TOTAL DE 'B'
+
+C      EQU   $0222	* CONTADOR DE ARTICULOS 'C'
+CD     EQU	 $0224  * DECENAS DE 'C'
+CC     EQU   $0225  * CENTENAS DE 'C'
+CUM    EQU   $0226  * UNIDADES DE MILLAR DE 'C'
+TC     EQU   $0227  * TOTAL DE 'C'
+
+D      EQU   $0232  * CONTADOR DE ARTICULOS 'D'
+DC     EQU	 $0234  * CENTENAS DE 'D'
+DUM    EQU   $0235  * UNIDADES DE MILLAR DE 'D'
+DDM    EQU   $0236  * DECENAS DE MILLAR DE 'D'
+TD     EQU   $0237  * TOTAL DE 'D'
+
+E      EQU   $0242  * CONTADOR DE ARTICULOS 'E'
+EC     EQU	 $0244  * CENTENAS DE 'E'
+EUM    EQU   $0245  * UNIDADES DE MILLAR DE 'E'
+EDM    EQU   $0246  * DECENAS DE MILLAR DE 'E'
+TE     EQU   $0247  * TOTAL DE 'E'
+
+F      EQU   $0252	* CONTADOR DE ARTICULOS 'F'
+FUM    EQU	 $0254  * UNIDADES DE MILLAR DE 'F'
+FDM    EQU   $0255  * DECENAS DE MILLAR DE 'F'
+FCM    EQU   $0256  * CENTENAS  DE MILLAR DE 'F'
+TF     EQU   $0257  * TOTAL DE 'F'
+
+G      EQU   $0262	* CONTADOR DE ARTICULOS 'G'
+GUM    EQU	 $0264  * UNIDADES DE MILLAR DE 'G'
+GDM    EQU   $0265  * DECENAS DE MILLAR DE 'G'
+GCM    EQU   $0266  * CENTENAS  DE MILLAR DE 'G'
+TG     EQU   $0267  * TOTAL DE 'G'
+
+H      EQU   $0272	* CONTADOR DE ARTICULOS 'H'
+HDM    EQU	 $0274  * DECENAS DE MILLAR DE 'H'
+HCM    EQU   $0275  * CENTENAS  DE MILLAR DE 'H'
+HUMI   EQU   $0276  * UNIDADES DE MILLON DE 'H'
+TH     EQU   $0277  * TOTAL DE 'H'
+
+I      EQU   $0282	* CONTADOR DE ARTICULOS 'I'
+IDM    EQU	 $0284  * DECENAS DE MILLAR DE 'I'
+ICM    EQU   $0285  * CENTENAS  DE MILLAR DE 'I'
+IUMI   EQU   $0286  * UNIDADES DE MILLON DE 'I'
+IDMI   EQU   $0287  * DECENAS DE MILLON DE 'I'
+TI     EQU   $0288  * TOTAL DE 'I'
+
+J      EQU   $0292  * CONTADOR DE ARTICULOS 'J' 
+JCM    EQU   $0294  * CENTENAS  DE MILLAR DE 'J'
+JUMI   EQU   $0295  * UNIDADES DE MILLON DE 'J'
+JDMI   EQU   $0296  * DECENAS DE MILLON DE 'J'
+TJ     EQU   $0297  * TOTAL DE 'J'
 			
-DECB   EQU   $0014	*la eplicación de las variables A es la misma para los otros 4 artículos
-B      EQU   $0017
-B1     EQU   $001B
-B2     EQU   $001C
-B3     EQU   $001D
-B4     EQU   $001E
-B5     EQU   $001F
-
-DECC   EQU   $0024
-C      EQU   $0027
-C1     EQU   $002B
-C2     EQU   $002C
-C3     EQU   $002D
-C4     EQU   $002E
-C5     EQU   $002F
-
-DECD   EQU   $0034
-D      EQU   $0037
-D1     EQU   $003B
-D2     EQU   $003C
-D3     EQU   $003D
-D4     EQU   $003E
-D5     EQU   $003F
-
-DECE   EQU   $0044
-E      EQU   $0047
-E1     EQU   $004B
-E2     EQU   $004C
-E3     EQU   $004D
-E4     EQU   $004E
-E5     EQU   $004F
-
-DECF   EQU   $0054
-F      EQU   $0057
-F1     EQU   $005B
-F2     EQU   $005C
-F3     EQU   $005D
-F4     EQU   $005E
-F5     EQU   $005F
-
-sA     EQU   $0009	*sA,sB,sC,sD,sE son los subtotales de cada artículo (en hexa)
-sB     EQU   $0019
-sC     EQU   $0029
-sD     EQU   $0039
-sE     EQU   $0049
-			
-ARTT   EQU   $0068	*ARTT es el total de artículos
+ARTT   EQU   $0300	* ARTT es el total de artículos
 	
-TOT1   EQU   $006B	*TOT1,...,TOT5 son usadas para representar el total en BCD (decenas, centenas, etc)
-TOT2   EQU   $006C
-TOT3   EQU   $006D
-TOT4   EQU   $006E
-TOT5   EQU   $006F
-
-GATO   EQU   $0010	*"Booleano" para detectar si se borrará algo	
-TEMP   EQU   $0020	*almacena lo que se va a aumentar
+UT     EQU   $0017	* TOTAL EN BCD (UNIDADES, DECENAS, CENTENAS, ...)
+DT     EQU   $0016
+CT     EQU   $0015
+UMT    EQU   $0014
+DMT    EQU   $0013
+CMT    EQU   $0012
+UMIT   EQU   $0011
+DMIT   EQU   $0010
+	
 TOTAL  EQU   $0050
-SIGNO  EQU   $0090
+SIGNO  EQU   $0002
 
 DIR_IMP EQU  $007B
 
@@ -116,11 +124,14 @@ DIR_IMP EQU  $007B
 *      VARIABLES PARA ERROR       *
 ***********************************
 
-ERR1   EQU   $0070 
-ERR2   EQU   $0071 
-ERR3   EQU   $0072 
-ERR4   EQU   $0073 
-ERR5   EQU   $0074 
+ERR1   EQU   $0030 
+ERR2   EQU   $0031 
+ERR3   EQU   $0032 
+ERR4   EQU   $0033 
+ERR5   EQU   $0034
+ERR6   EQU   $0035
+ERR7   EQU   $0036
+ERR8   EQU   $0037 
 BCD1   EQU   $0060 
 BCD2   EQU   $0061 
 BCD3   EQU   $0062 
@@ -169,59 +180,70 @@ INICIO
     LDS   #$00EE  
     JSR   SERIAL
 
-LIMPIA			
-	CLR  A
-	CLR  AU
-	CLR  AD
-	CLR  AC
-	CLR  AUM
-	CLR  ADM
-	CLR  ACM
-	CLR  AUMI
-	CLR  ADMI
-	CLR  B
-	CLR  B1
-	CLR  B2
-	CLR  B3
-	CLR  B4
-	CLR  B5
-	CLR  C
-	CLR  C1
-	CLR  C2
-	CLR  C3
-	CLR  C4
-	CLR  C5
-	CLR  D
-	CLR  D1
-	CLR  D2
-	CLR  D3
-	CLR  D4
-	CLR  D5
-	CLR  E
-	CLR  E1
-	CLR  E2
-	CLR  E3
-	CLR  E4
-	CLR  E5
-	CLR  TOT1
-	CLR  TOT2
-	CLR  TOT3
-	CLR  TOT4
-	CLR  TOT5
-	CLR  DECA
-	CLR  DECB
-	CLR  DECC
-	CLR  DECD
-	CLR  DECE
-	CLR  sA
-	CLR  sB
-	CLR  sC
-	CLR  sD
-	CLR  sE
-	CLR  GATO
-	CLR  TOTAL
-	CLR  SIGNO
+LIMPIAR		
+	CLR A
+	CLR AU
+	CLR AD
+	CLR AC
+	CLR TA
+	CLR B
+	CLR BD
+	CLR BC
+	CLR BUM
+	CLR TB
+	CLR C
+	CLR CD
+	CLR CC
+	CLR CUM
+	CLR TC
+	CLR D
+	CLR DC
+	CLR DUM
+	CLR DDM
+	CLR TD
+	CLR E
+	CLR EC
+	CLR EUM
+	CLR EDM
+	CLR TE
+	CLR F
+	CLR FUM
+	CLR FDM
+	CLR FCM
+	CLR TF
+	CLR G
+	CLR GUM
+	CLR GDM
+	CLR GCM
+	CLR TG
+	CLR H
+	CLR HDM
+	CLR HCM
+	CLR HUMI
+	CLR TH
+	CLR I
+	CLR IDM
+	CLR ICM
+	CLR IUMI
+	CLR IDMI
+	CLR TI
+	CLR J
+	CLR JCM
+	CLR JUMI
+	CLR JDMI
+	CLR TJ
+	CLR UT
+	CLR DT
+	CLR CT
+	CLR UMT
+	CLR DMT
+	CLR CMT
+	CLR UMIT
+	CLR DMIT
+	CLR TOTAL
 
+	LDAB #'+
+	STAB SIGNO
 ENCICLATE
 	LDAA #'?
 	STAA ORDEN
@@ -242,12 +264,12 @@ CICLO
 	BEQ MODIFICAR_D
 	CMPA #'E
 	BEQ MODIFICAR_E
+	CMPA #'F'
+	BEQ MODIFICAR_F
 	JMP ENCICLATE
 
 MODIFICAR_A
 	CMPB #'+
-	BEQ AGREGAR_A
-	CMPB #0
 	BEQ AGREGAR_A
 	CMPB #'-
 	BNE ENCICLATE
@@ -256,16 +278,12 @@ MODIFICAR_A
 MODIFICAR_B
 	CMPB #'+
 	BEQ AGREGAR_B
-	CMPB #0
-	BEQ AGREGAR_B
 	CMPB #'-
 	BNE ENCICLATE
 	JMP QUITAR_B
 
 MODIFICAR_C
 	CMPB #'+
-	BEQ AGREGAR_C
-	CMPB #0
 	BEQ AGREGAR_C
 	CMPB #'-
 	BNE ENCICLATE
@@ -274,16 +292,12 @@ MODIFICAR_C
 MODIFICAR_D
 	CMPB #'+
 	BEQ AGREGAR_D
-	CMPB #0
-	BEQ AGREGAR_D
 	CMPB #'-
 	BNE ENCICLATE
-	JMP QUITAR_C
+	JMP QUITAR_D
 
 MODIFICAR_E
 	CMPB #'+
-	BEQ AGREGAR_E
-	CMPB #0
 	BEQ AGREGAR_E
 	CMPB #'-
 	BNE ENCICLATE
@@ -291,8 +305,6 @@ MODIFICAR_E
 
 MODIFICAR_F
 	CMPB #'+
-	BEQ AGREGAR_F
-	CMPB #0
 	BEQ AGREGAR_F
 	CMPB #'-
 	BNE ENCICLATE
@@ -386,52 +398,42 @@ CALCULAR
 * SUBRUTINA  DE CONFIGURACION PUERTO SERIAL      *
 **************************************************
 SERIAL
-       LDD   #$302C  * CONFIGURA PUERTO SERIAL
-       STAA  BAUD    * BAUD  9600  para cristal de 8MHz
-       STAB  SCCR2   * HABILITA  RX Y TX PERO INTERRUPCN SOLO RX
-       LDAA  #$00
-       STAA  SCCR1   * 8 BITS
+    LDD   #$302C  * CONFIGURA PUERTO SERIAL
+    STAA  BAUD    * BAUD  9600  para cristal de 8MHz
+    STAB  SCCR2   * HABILITA  RX Y TX PERO INTERRUPCN SOLO RX
+    LDAA  #$00
+    STAA  SCCR1   * 8 BITS
 
-       LDAA  #$FE    * CONFIG PUERTO D COMO SALIDAS (EXCEPTO PD0)
-       STAA  DDRD    * SEA  ENABLE DEL DISPLAY  PD4  Y RS PD3
-                     
+    LDAA  #$FE    * CONFIG PUERTO D COMO SALIDAS (EXCEPTO PD0)
+    STAA  DDRD    * SEA  ENABLE DEL DISPLAY  PD4  Y RS PD3                    
       
-       LDAA  #$04
-       STAA  HPRIO
+    LDAA  #$04
+    STAA  HPRIO
 
-       LDAA  #$00
-       TAP
-       RTS
-
+    LDAA  #$00
+    TAP
+    RTS
 ***********************************
 * ATENCION A INTERRUPCION SERIAL
 ***********************************
-       ORG  $F100
+    ORG  $F100
   
-       
- 
-       LDAA SCSR
-       LDAA SCDR
-       STAA ORDEN
-       DEC  VAR
-
-        
-
-       RTI
-
+    LDAA SCSR
+    LDAA SCDR
+    STAA ORDEN
+    DEC  VAR
+    RTI
 
 ***********************************
 * VECTOR INTERRUPCION SERIAL
 ***********************************
-       ORG   $FFD6
-       FCB   $F1,$00       
-
- 
+    ORG   $FFD6
+    FCB   $F1,$00       
 
 ***********************************
 *RESET
 ***********************************
-       ORG    $FFFE
+    ORG    $FFFE
 RESET  FCB    $80,$00
 ***********************************
 
@@ -439,15 +441,15 @@ RESET  FCB    $80,$00
 ********
 ***TODO LO SIGUIENTE USA LA DIRECTIVA FCC PARA ESCRIBIR LA INFROAMCIÓN DEL TICKET, AL ESTAR AL FINAL NO TIENE EL PROBLEMA DE IMPRIMIR LOS OPCODES
 ********
-	ORG   $3000
+	ORG   $0040
 MSG1	FCC   'TICKET'
-	ORG   $3010
+	ORG   $0050
 MSG2	FCC   '----------------'
-	ORG   $30C0
+	ORG   $0100
 MSG3	FCC   '----------------'
-	ORG   $30D0
+	ORG   $0110
 MSG4	FCC   'TOTAL:'
-	ORG   $30E4
+	ORG   $0124
 MSG5	FCC   'PIEZAS'
 
 	END
